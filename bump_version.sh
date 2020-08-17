54007462bda98f188a9da2d41aa9d3621a44238c
Carmi Grushko <carmi@fb.com>
2020-08-17 08:12:50 -0700

    ./bump_version.sh handles ktfmt_idea_plugin
    Reviewed By: hick209
    
    Differential Revision: D23154114
    
    fbshipit-source-id: 393beda7bdf6fd9487ff3f1746ff07c52be75d06

--- bump_version.sh
+++ bump_version.sh
@@ -13,16 +13,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo "Edit the file before running it."
-exit # <--- comment out after updating the versions below.
+display_usage() {
+    echo "Usage:"
+    echo "/bump_version.sh CURRENT_VERSION NEXT_VERSION"
+    echo
+    echo "Example:"
+    echo "./bump_version.sh 0.19 0.20"
+}
 
-CURRENT_VERSION="0.13"
-NEXT_VERSION="0.14"
+if [  $# -le 1 ]
+then
+    display_usage
+    exit 1
+fi
+
+CURRENT_VERSION="$1"
+NEXT_VERSION="$2"
 
 POM1="$HOME/fbsource/xplat/ktfmt/pom.xml"
 POM2="$HOME/fbsource/xplat/ktfmt/core/pom.xml"
+BUILD_GRADLE="$HOME/fbsource/xplat/ktfmt/ktfmt_idea_plugin/build.gradle.kts"
 
 sed -i.bak "s/$CURRENT_VERSION-SNAPSHOT/$CURRENT_VERSION/g" "$POM1" "$POM2"
-hg commit -m "[ktfmt] Bump version to $CURRENT_VERSION" "$POM1" "$POM2"
+sed -i.bak "s/val ktfmtVersion = \"$CURRENT_VERSION-SNAPSHOT\"/val ktfmtVersion = \"$CURRENT_VERSION\"/g" "$BUILD_GRADLE"
+hg commit -m "[ktfmt] Bump version to $CURRENT_VERSION" "$POM1" "$POM2" "$BUILD_GRADLE"
+
 sed -i.bak "s/$CURRENT_VERSION/$NEXT_VERSION-SNAPSHOT/g" "$POM1" "$POM2"
-hg commit -m "[ktfmt] Bump version to $NEXT_VERSION-SNAPSHOT" "$POM1" "$POM2"
+sed -i.bak "s/val ktfmtVersion = \"$CURRENT_VERSION\"/val ktfmtVersion = \"$NEXT_VERSION-SNAPSHOT\"/g" "$BUILD_GRADLE"
+hg commit -m "[ktfmt] Bump version to $NEXT_VERSION-SNAPSHOT" "$POM1" "$POM2" "$BUILD_GRADLE"
+
+rm "$POM1.bak" "$POM2.bak" "$BUILD_GRADLE.bak"
