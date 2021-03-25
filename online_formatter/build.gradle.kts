6a6ee5d583e3d04b13db2cdd75c98722d5f469df
Carmi Grushko <carmi@fb.com>
2021-03-25 12:55:01 -0700

    AWS Lambda that formats code in a request (#198)
    Summary: Pull Request resolved: https://github.com/facebookincubator/ktfmt/pull/198
    
    Reviewed By: hick209
    
    Differential Revision: D27163581
    
    fbshipit-source-id: 1b97f219b87027f250063e8e198c6a3c419557f5

---
+++ online_formatter/build.gradle.kts
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins { kotlin("jvm") version "1.4.21" }
+
+repositories {
+  mavenLocal()
+  mavenCentral()
+}
+
+val ktfmtVersion = rootProject.file("../version.txt").readText().trim()
+
+dependencies {
+  implementation("com.facebook:ktfmt:$ktfmtVersion")
+  implementation(platform("software.amazon.awssdk:bom:2.10.73"))
+  implementation("software.amazon.awssdk:lambda")
+  implementation("com.amazonaws:aws-lambda-java-core:1.2.1")
+  implementation("com.amazonaws:aws-lambda-java-events:2.2.9")
+  implementation("com.google.code.gson:gson:2.8.6")
+  testImplementation(kotlin("test-junit"))
+}
+
+tasks {
+  test { useJUnit() }
+
+  withType<KotlinCompile>() { kotlinOptions.jvmTarget = "11" }
+
+  val packageFat by creating(Zip::class) {
+    from(compileKotlin)
+    from(processResources)
+    into("lib") { from(configurations.runtimeClasspath) }
+    dirMode = 0b111101101 // 0755
+    fileMode = 0b111101101 // 0755
+  }
+
+  val packageLibs by creating(Zip::class) {
+    into("java/lib") { from(configurations.runtimeClasspath) }
+    dirMode = 0b111101101 // 0755
+    fileMode = 0b111101101 // 0755
+  }
+
+  val packageSkinny by creating(Zip::class) {
+    from(compileKotlin)
+    from(processResources)
+  }
+
+  build { dependsOn(packageSkinny) }
+}
---
+++ online_formatter/src/main/kotlin/main.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.facebook.ktfmt.onlineformatter
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import com.facebook.ktfmt.KOTLINLANG_FORMAT
+import com.facebook.ktfmt.format
+import com.facebook.ktfmt.parseOptions
+import com.google.gson.Gson
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+class Handler : RequestHandler<APIGatewayProxyRequestEvent, String> {
+  init {
+    // Warm up.
+    gson.toJson(format(KOTLINLANG_FORMAT, "/* foo */ fun foo() {}"))
+  }
+
+  override fun handleRequest(event: APIGatewayProxyRequestEvent, context: Context?): String {
+    return gson.toJson(
+        try {
+          val request = gson.fromJson(event.body, Request::class.java)
+          val parsingErrors = ByteArrayOutputStream()
+          val style = request.style
+          val parsedArgs =
+              parseOptions(
+                  PrintStream(parsingErrors), if (style == null) arrayOf() else arrayOf(style))
+          Response(
+              format(
+                  parsedArgs.formattingOptions.copy(maxWidth = request.maxWidth ?: 100),
+                  request.source ?: ""),
+              parsingErrors.toString().ifEmpty { null })
+        } catch (e: Exception) {
+          Response(null, e.message)
+        })
+  }
+
+  companion object {
+    val gson = Gson()
+  }
+}
+
+class Request {
+  var source: String? = ""
+  var style: String? = ""
+  var maxWidth: Int? = 100
+}
+
+data class Response(val source: String?, val err: String?)
