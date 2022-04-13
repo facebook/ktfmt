1bebf0bc4677832a2b7004acf42a938baecd565a
Beth Cutler <bcutler@google.com>
2022-04-13 08:17:20 -0700

    Add trailing break before brace for Google Style (#308)
    Summary:
    Fix for https://github.com/facebookincubator/ktfmt/issues/245
    
    Pull Request resolved: https://github.com/facebookincubator/ktfmt/pull/308
    
    Reviewed By: cgrushko
    
    Differential Revision: D35506462
    
    Pulled By: strulovich
    
    fbshipit-source-id: 5419130214152e6e083919935e5ae3be0cb47492

--- core/src/test/java/com/facebook/ktfmt/format/GoogleStyleFormatterKtTest.kt
+++ core/src/test/java/com/facebook/ktfmt/format/GoogleStyleFormatterKtTest.kt
@@ -483,15 +483,15 @@ class GoogleStyleFormatterKtTest {
       |    something
       |      is
       |      PairList<
-      |        String,
-      |        Int>
+      |        String, Int
+      |      >
       |  )
       |  doIt(
       |    something
       |      as
       |      PairList<
-      |        String,
-      |        Int>
+      |        String, Int
+      |      >
       |  )
       |  println(
       |    a is Int &&
@@ -830,6 +830,54 @@ class GoogleStyleFormatterKtTest {
       |      boo
       |    )
       |}
+      |""".trimMargin(),
+          formattingOptions = Formatter.GOOGLE_FORMAT,
+          deduceMaxWidth = true)
+
+  @Test
+  fun `trailing break argument list`() =
+      assertFormatted(
+          """
+      |-------------------
+      |fun method() {
+      |  Foo.FooBar(
+      |    longParameter
+      |  )
+      |  Foo.FooBar(
+      |    param1,
+      |    param2
+      |  )
+      |}
+      |""".trimMargin(),
+          formattingOptions = Formatter.GOOGLE_FORMAT,
+          deduceMaxWidth = true)
+
+  @Test
+  fun `trailing break chains`() =
+      assertFormatted(
+          """
+      |-------------
+      |bar(
+      |  FooOpClass
+      |    .doOp(1)
+      |    .doOp(2)
+      |)
+      |""".trimMargin(),
+          formattingOptions = Formatter.GOOGLE_FORMAT,
+          deduceMaxWidth = true)
+
+  @Test
+  fun `wrapping for long function types`() =
+      assertFormatted(
+          """
+      |------------------------
+      |var listener:
+      |  (
+      |    a: String,
+      |    b: String,
+      |    c: String,
+      |    d: String
+      |  ) -> Unit
       |""".trimMargin(),
           formattingOptions = Formatter.GOOGLE_FORMAT,
           deduceMaxWidth = true)
