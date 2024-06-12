c119d160e10159ccf1d0e2c546248e5e4cd61af4
Nivaldo Bondança <nbondanca@meta.com>
2024-06-12 04:42:41 -0700

    Created script to help with testing rest of ktfmt
    Summary: Since we often forget to test things here, I'm creating this scripting and pluging it into our buck testing infra. Hopefully this
    
    Reviewed By: cortinico
    
    Differential Revision: D58448053
    
    fbshipit-source-id: 746697888c7f1572f60262a0396bada00d3a1055

---
+++ smoke_tests.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+# Absolute path to this script, e.g. /home/user/ktfmt/smoke_tests.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/ktfmt/
+ROOT_DIR=$(dirname "$SCRIPT")
+
+echo "Testing ktfmt core library"
+echo
+cd "$ROOT_DIR"
+mvn clean install
+echo
+
+echo "Testing ktfmt IDEA plugin"
+echo
+cd "$ROOT_DIR/ktfmt_idea_plugin"
+./gradlew :build
+echo
+
+echo "Testing online formatter"
+echo
+cd "$ROOT_DIR/online_formatter"
+./gradlew :build
+echo
