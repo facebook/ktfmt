e02804d3658438af2782ec85c9de32d18e28cfc9
Zac Sweers <pandanomic@gmail.com>
2025-05-09 19:46:23 -0700

    Support guard conditions (#537)
    Summary:
    Resolves https://github.com/facebook/ktfmt/issues/530
    
    Pull Request resolved: https://github.com/facebook/ktfmt/pull/537
    
    Reviewed By: navidqar
    
    Differential Revision: D74476924
    
    Pulled By: hick209
    
    fbshipit-source-id: afc8599b723f9a9f808d68b1a35a9ae81c07ced3

--- core/src/test/java/com/facebook/ktfmt/format/TokenizerTest.kt
+++ core/src/test/java/com/facebook/ktfmt/format/TokenizerTest.kt
@@ -101,17 +101,16 @@ class TokenizerTest {
       |val a=5
       |"""
             .trimMargin()
-            .trimMargin()
 
     val file = Parser.parse(code)
     val tokenizer = Tokenizer(code, file)
     file.accept(tokenizer)
 
     assertThat(tokenizer.toks.map { it.originalText })
-        .containsExactly("val", " ", "b", "=", "\"a\"", "\n", "val", " ", "a", "=", "5")
+        .containsExactly("val", " ", "b", "=", "\"a\"", "\n", "val", " ", "a", "=", "5", "\n")
         .inOrder()
     assertThat(tokenizer.toks.map { it.index })
-        .containsExactly(0, -1, 1, 2, 3, -1, 4, -1, 5, 6, 7)
+        .containsExactly(0, -1, 1, 2, 3, -1, 4, -1, 5, 6, 7, -1)
         .inOrder()
   }
 
@@ -127,7 +126,6 @@ class TokenizerTest {
       |  fun test() {}
       |}
       |"""
-            .trimMargin()
             .trimMargin()
 
     val file = Parser.parse(code)
@@ -174,7 +172,8 @@ class TokenizerTest {
             "{",
             "}",
             "\n",
-            "}")
+            "}",
+            "\n")
         .inOrder()
     assertThat(tokenizer.toks.map { it.index })
         .containsExactly(
@@ -216,7 +215,129 @@ class TokenizerTest {
             21,
             22,
             -1,
-            23)
+            23,
+            -1)
+        .inOrder()
+  }
+
+  @Test
+  fun `Guard conditions with subject are parsed correctly`() {
+    // language=kotlin
+    val code =
+        """
+      |fun feedAnimal(animal: Animal) {
+      |    when (animal) {
+      |        is Animal.Cat if !animal.mouseHunter -> animal.feedCat()
+      |    }
+      |}
+      |"""
+            .trimMargin()
+
+    val file = Parser.parse(code)
+    val tokenizer = Tokenizer(code, file)
+    file.accept(tokenizer)
+
+    assertThat(tokenizer.toks.map { it.originalText })
+        .containsExactly(
+            "fun",
+            " ",
+            "feedAnimal",
+            "(",
+            "animal",
+            ":",
+            " ",
+            "Animal",
+            ")",
+            " ",
+            "{",
+            "\n",
+            "    ",
+            "when",
+            " ",
+            "(",
+            "animal",
+            ")",
+            " ",
+            "{",
+            "\n",
+            "        ",
+            "is",
+            " ",
+            "Animal",
+            ".",
+            "Cat",
+            " ",
+            "if",
+            " ",
+            "!",
+            "animal",
+            ".",
+            "mouseHunter",
+            " ",
+            "->",
+            " ",
+            "animal",
+            ".",
+            "feedCat",
+            "(",
+            ")",
+            "\n",
+            "    ",
+            "}",
+            "\n",
+            "}",
+            "\n")
+        .inOrder()
+    assertThat(tokenizer.toks.map { it.index })
+        .containsExactly(
+            0,
+            -1,
+            1,
+            2,
+            3,
+            4,
+            -1,
+            5,
+            6,
+            -1,
+            7,
+            -1,
+            -1,
+            8,
+            -1,
+            9,
+            10,
+            11,
+            -1,
+            12,
+            -1,
+            -1,
+            13,
+            -1,
+            14,
+            15,
+            16,
+            -1,
+            17,
+            -1,
+            18,
+            19,
+            20,
+            21,
+            -1,
+            22,
+            -1,
+            23,
+            24,
+            25,
+            26,
+            27,
+            -1,
+            -1,
+            28,
+            -1,
+            29,
+            -1)
         .inOrder()
   }
 
